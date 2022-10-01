984e5504ee69748d913395607e35fa8aba966434
Júnior Nunes <denisonprobable13@gmail.com>
2022-10-01 17:51:07 -0300

    feat: route get created

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -2,11 +2,18 @@ import { Request, Response } from "express";
 
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
+interface IHeaders {
+  user_id: string;
+}
+
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = (request.headers as unknown) as IHeaders;
+    const users = this.listAllUsersUseCase.execute({ user_id });
+
+    return response.json(200).send(users);
   }
 }
 
