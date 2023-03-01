34684f75d7b40f43ec7c8a0306bf1bc75a5ccf90
Júnior Nunes <denisonprobable13@gmail.com>
2023-03-01 12:11:42 -0300

    feat: swagger config initialized

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
+import swaggerUI from "swagger-ui-express";
+
 import { app } from ".";
+import swaggerfile from "./swagger.json";
 
 app.listen(3333, () => console.log("Server is running!"));
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerfile));
